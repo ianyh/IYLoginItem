a0e65873eed28285c7ec1e65aaa8e7696236ebf3
Ian Ynda-Hummel <ianynda@gmail.com>
2013-06-08 18:56:59 -0400

    Fill out bundle category logic.

--- IYLoginItem/NSBundle+LoginItem.m
+++ IYLoginItem/NSBundle+LoginItem.m
@@ -8,18 +8,67 @@
 
 #import "NSBundle+LoginItem.h"
 
+#import <CoreServices/CoreServices.h>
+
+@interface NSBundle (LoginItemPrivate)
+- (LSSharedFileListItemRef)itemRefWithListRef:(LSSharedFileListRef)listRef;
+@end
+
 @implementation NSBundle (LoginItem)
 
 - (BOOL)isLoginItem {
-    return NO;
+    LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL, kLSSharedFileListSessionLoginItems, NULL);
+    if (!loginItems) return NO;
+    
+    LSSharedFileListItemRef loginItemRef = [self itemRefWithListRef:loginItems];
+    if (!loginItemRef) return NO;
+
+    return YES;
 }
 
 - (void)addToLoginItems {
+    NSURL *bundleURL = self.bundleURL;
+
+    LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL, kLSSharedFileListSessionLoginItems, NULL);
+    if (!loginItems) return;
     
+    LSSharedFileListItemRef item = LSSharedFileListInsertItemURL(loginItems,
+                                                                 kLSSharedFileListItemLast,
+                                                                 NULL,
+                                                                 NULL,
+                                                                 (__bridge CFURLRef)bundleURL,
+                                                                 NULL,
+                                                                 NULL);
+
+    if (item) CFRelease(item);
+
+    CFRelease(loginItems);
 }
 
 - (void)removeFromLoginItems {
-    
+    LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL, kLSSharedFileListSessionLoginItems, NULL);
+    if (!loginItems) return;
+
+    LSSharedFileListItemRef loginItemRef = [self itemRefWithListRef:loginItems];
+    if (!loginItemRef) return;
+
+    LSSharedFileListItemRemove(loginItems, loginItemRef);
+}
+
+- (LSSharedFileListItemRef)itemRefWithListRef:(LSSharedFileListRef)listRef {
+    NSArray *listItems = (__bridge NSArray *)LSSharedFileListCopySnapshot(listRef, NULL);
+
+    for (NSInteger i = 0; i < listItems.count; ++i) {
+        LSSharedFileListItemRef itemRef = (__bridge LSSharedFileListItemRef)listItems[i];
+        CFURLRef urlRef;
+        OSStatus error = LSSharedFileListItemResolve(itemRef, 0, &urlRef, NULL);
+        
+        if (error != noErr) continue;
+        
+        if (CFEqual(urlRef, (__bridge CFURLRef)self.bundleURL)) return itemRef;
+    }
+
+    return NULL;
 }
 
 @end
