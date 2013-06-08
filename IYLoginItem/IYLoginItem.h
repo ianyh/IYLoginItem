f849bf00109b3056cc9ff7c12bccacef63c1b735
Ian Ynda-Hummel <ianynda@gmail.com>
2013-06-08 15:53:14 -0400

    Initial Commit

---
+++ IYLoginItem/IYLoginItem.h
@@ -0,0 +1,13 @@
+//
+//  IYLoginItem.h
+//  IYLoginItem
+//
+//  Created by Ian Ynda-Hummel on 6/8/13.
+//  Copyright (c) 2013 Ian Ynda-Hummel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IYLoginItem : NSObject
+
+@end
