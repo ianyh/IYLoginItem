ddf844c3a1cb969f85a200192b2789a6ff903ffe
Ian Ynda-Hummel <ianynda@gmail.com>
2013-06-08 18:59:15 -0400

    Add a unit test target.

---
+++ IYLoginItemTests/IYLoginItemTests.h
@@ -0,0 +1,13 @@
+//
+//  IYLoginItemTests.h
+//  IYLoginItemTests
+//
+//  Created by Ian Ynda-Hummel on 6/8/13.
+//  Copyright (c) 2013 Ian Ynda-Hummel. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface IYLoginItemTests : SenTestCase
+
+@end
---
+++ IYLoginItemTests/IYLoginItemTests.m
@@ -0,0 +1,32 @@
+//
+//  IYLoginItemTests.m
+//  IYLoginItemTests
+//
+//  Created by Ian Ynda-Hummel on 6/8/13.
+//  Copyright (c) 2013 Ian Ynda-Hummel. All rights reserved.
+//
+
+#import "IYLoginItemTests.h"
+
+@implementation IYLoginItemTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in IYLoginItemTests");
+}
+
+@end
