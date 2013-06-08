558fe1406f2eebd2608e43a94f53b6086e3a30c2
Ian Ynda-Hummel <ianynda@gmail.com>
2013-06-08 19:09:32 -0400

    Add some documentation.

--- IYLoginItem/NSBundle+LoginItem.h
+++ IYLoginItem/NSBundle+LoginItem.h
@@ -10,8 +10,18 @@
 
 @interface NSBundle (LoginItem)
 
+// Returns YES if the bundle is a login item such that it will be launched
+// every time the currently logged in user logs in.
 - (BOOL)isLoginItem;
+
+// Takes the bundle and adds it as a login item such that it will be launched
+// every time the currently logged in user logs in. If the bundle is already a
+// login item this method has no additional effect.
 - (void)addToLoginItems;
+
+// Takes the bundle and removes it from login items such that it will not be
+// launched every time the currently logged in user logs in. If the bundle is
+// not a login item this method has no additional effect.
 - (void)removeFromLoginItems;
 
 @end
